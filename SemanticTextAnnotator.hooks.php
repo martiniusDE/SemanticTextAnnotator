4892a1925629527eaf0813b98d261aa1b8faa5c5
m.liebeskind <martin.liebeskind@invia.de>
2019-01-03 14:54:20 +0100

    added tweekie support

--- SemanticTextAnnotator.hooks.php
+++ SemanticTextAnnotator.hooks.php
@@ -6,21 +6,22 @@
  * @ingroup Extensions
  */
 
-class SemanticTextAnnotatorHooks {
+class SemanticTextAnnotatorHooks
+{
 
-	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
-		if($out->getTitle()->getNamespace() == 0) {		//0 is the Aricle's Namespace
-			$out->addModules( 'ext.annotator' );
-		}
-		return true;
-	}
+    public static function onBeforePageDisplay(OutputPage &$out, Skin &$skin)
+    {
+        $out->addModules('ext.annotator');
+        return true;
+    }
 
-	public static function onCanonicalNamespaces( array &$namespaces ) {
+    public static function onCanonicalNamespaces(array &$namespaces)
+    {
         global $wgNamespacesWithSubpages;
 
-        if ( !defined( 'NS_ANNOTATION' ) ) {
-            define( 'NS_ANNOTATION', 248 );
-            define( 'NS_ANNOTATION_TALK', 249 );
+        if (!defined('NS_ANNOTATION')) {
+            define('NS_ANNOTATION', 248);
+            define('NS_ANNOTATION_TALK', 249);
         }
 
         $namespaces[NS_ANNOTATION] = 'Annotation';
@@ -31,12 +32,13 @@ class SemanticTextAnnotatorHooks {
         return true;
     }
 
-    public static function afterInit( ) {
+    public static function afterInit()
+    {
         global $smwgNamespacesWithSemanticLinks;
 
-        if ( !defined( 'NS_ANNOTATION' ) ) {
-            define( 'NS_ANNOTATION', 248 );
-            define( 'NS_ANNOTATION_TALK', 249 );
+        if (!defined('NS_ANNOTATION')) {
+            define('NS_ANNOTATION', 248);
+            define('NS_ANNOTATION_TALK', 249);
         }
 
         $smwgNamespacesWithSemanticLinks[NS_ANNOTATION] = true;
@@ -47,22 +49,22 @@ class SemanticTextAnnotatorHooks {
         return true;
     }
 
-    public static function smwInitProperties( ) {
-        if ( class_exists( 'SMW\PropertyRegistry' ) ) {
-            SMW\PropertyRegistry::getInstance()->registerProperty ( "___SA_FORM_TYPE_PROPERTY", '_txt', "Form Type", true, true );
-            SMW\PropertyRegistry::getInstance()->registerProperty ( "___SA_CATEGORY_NAME_PROPERTY", '_txt', "SA Category Name", true, true );
-            SMW\PropertyRegistry::getInstance()->registerProperty ( "___SA_CATEGORY_COLOR_PROPERTY", '_txt', "SA Category Color", true, true );
+    public static function smwInitProperties()
+    {
+        if (class_exists('SMW\PropertyRegistry')) {
+            SMW\PropertyRegistry::getInstance()->registerProperty("___SA_FORM_TYPE_PROPERTY", '_txt', "Form Type", true, true);
+            SMW\PropertyRegistry::getInstance()->registerProperty("___SA_CATEGORY_NAME_PROPERTY", '_txt', "SA Category Name", true, true);
+            SMW\PropertyRegistry::getInstance()->registerProperty("___SA_CATEGORY_COLOR_PROPERTY", '_txt', "SA Category Color", true, true);
         }
-        /*echo ""+SMW\PropertyRegistry::getInstance 	( );
-        /*SMW\PropertyRegistry::registerProperty 	($id,
-            $typeId,
-            $label = false,
-            $isVisible = false,
-            $isAnnotable = true
+        /*echo ""+SMW\PropertyRegistry::getInstance     ( );
+        /*SMW\PropertyRegistry::registerProperty     ($id,
+        $typeId,
+        $label = false,
+        $isVisible = false,
+        $isAnnotable = true
         );*/
 
         return true;
     }
-
 
 }
--- modules/ext.annotator.js
+++ modules/ext.annotator.js
@@ -5,38 +5,47 @@
 // flag representing the status of annotator mode
 var loaded = false;
 var installed = false;
-(function() {
-        api.existPage('Form:TextAnnotation', function(content) {
-            if (content) {
-                installed = true;
-            }
-        });
-        // append annotate button and status to menu
-        if (mw.config.get('wgAction') == 'view') {
-            $('#p-views>ul').append('<li id="ca-annotate"><span><a href="#" title="' + mw.msg('sta-button-desc') + '" accesskey="a">' + mw.msg('sta-button-text') + '</a><i class="fa fa-check" aria-hidden="true"></i></span></li>');
-            // do if annotate button is clicked
-            $('#ca-annotate').click(function() {
-                    $('body').append('<div class="annotator-loading"><div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div>');
-                    loaded = !loaded;
+(function () {
+	api.existPage('Form:TextAnnotation', function (content) {
+		if (content) {
+			installed = true;
+		}
+	});
+	// append annotate button and status to menu
+	if (mw.config.get('wgAction') == 'view') {
 
-                    if (loaded && installed) {
-                        mw.loader.using('ext.annotator.module').then(function() {
-                            $('#ca-annotate').addClass('selected');
-                            // if module is loaded message will pop up
-                            mw.notify(mw.message('sta-welcome-message'));
-                        });
-                    } else if (!installed) {
-                        mw.notify(mw.message('sta-please-install'));
-                        $('.annotator-loading').hide();
-                    } else {
-                        //$('#content').annotator('destroy');
-                        $('#ca-annotate').removeClass('selected');
-                        mw.notify(mw.message('sta-godbye-message'));
-                        location.reload();
-                    }
+		// try tweekie skin
+		if ($('body').hasClass('tweeki-animateLayout')) {
+			// add to sidebar
+			$('#sidebar-right').append('<div class="btn-group btn-block"><a href="#" id="ca-annotate" class="btn btn-primary btn-block" title="' + mw.msg('sta-button-desc') + '" accesskey="a">' + mw.msg('sta-button-text') + '</a></div>');
+		} else {
+			// default skin (vector)
+			$('#p-views>ul').append('<li id="ca-annotate"><span><a href="#" title="' + mw.msg('sta-button-desc') + '" accesskey="a">' + mw.msg('sta-button-text') + '</a><i class="fa fa-check" aria-hidden="true"></i></span></li>');
+		}
+
+		// do if annotate button is clicked
+		$('#ca-annotate').click(function () {
+			$('body').append('<div class="annotator-loading"><div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div>');
+			loaded = !loaded;
+
+			if (loaded && installed) {
+				mw.loader.using('ext.annotator.module').then(function () {
+					$('#ca-annotate').addClass('selected');
+					// if module is loaded message will pop up
+					mw.notify(mw.message('sta-welcome-message'));
+				});
+			} else if (!installed) {
+				mw.notify(mw.message('sta-please-install'));
+				$('.annotator-loading').hide();
+			} else {
+				//$('#content').annotator('destroy');
+				$('#ca-annotate').removeClass('selected');
+				mw.notify(mw.message('sta-godbye-message'));
+				location.reload();
+			}
 
 
-            });
-        }
+		});
+	}
 
 }());
